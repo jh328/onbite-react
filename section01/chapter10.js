f6d6995ea76e65aaf7cd24a67ebc74cce231877f
jh328 <wnsgks32810@naver.com>
2024-12-04 00:43:18 +0900

    second react commit

--- section01/chapter10.js
+++ section01/chapter10.js
@@ -12,7 +12,7 @@ for (let index = 0; index<5; index++){
 for (let index = 1; index <= 10; index++) {
   console.log("몇번 증감을 했는지 확인하는 로그 : ", index);
 
-  if(index >=5){
+  if(index >=5){https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FdV5Gd3%2FbtrRjWybb4X%2F2kuIVR5KttJUK2DyNtmDL1%2Fimg.png
     console.log("멈춰~");
     break
   }
--- section03/src/math.js
+++ section03/src/math.js
@@ -15,8 +15,8 @@ module.exports = {
 이 코드가 CommonJS 코드이다.   
 */
 
-// export {add,sub}; // module로 값을 보낼 때 객체를 리터럴로 생성을 해서 보내주면 된다. 
+// export {add,sub}; // module로 값을 보낼 때 객체를 리터럴로 생성을 해서 보내주면 된다.
 
-export default function multiply(a,b){
-    return a * b;
+export default function multiply(a, b) {
+  return a * b;
 }
