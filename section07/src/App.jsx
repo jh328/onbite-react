70b156c54a7bfc69b4398523aa97a8a2d2fcaaab
jh328 <wnsgks32810@naver.com>
2024-12-10 13:01:10 +0900

    section07 END

---
+++ section07/src/App.jsx
@@ -0,0 +1,52 @@
+import "./App.css";
+import Viewer from "./components/Viewer";
+import Controller from "./components/Controller";
+import { useState, useEffect, useRef } from "react";
+import Even from "./components/Even";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+  const isMount = useRef(false);
+  // 1. Mount : 탄생
+  useEffect(() => {
+    console.log("mount");
+  }, []);
+  // 2. Update : 변화, 리렌더링
+  useEffect(() => {
+    if (!isMount.current) {
+      isMount.current = true;
+      return;
+    }
+    console.log("update");
+  }); // 뎁스를 생략,
+
+  // 3. UnMount : 죽음
+
+  const onClickButton = (value) => {
+    setCount(count + value);
+  };
+  return (
+    <div className="App">
+      <h1>Simple Counter</h1>
+      <section>
+        <input
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+      </section>
+      <section>
+        <Viewer count={count} />
+        {count % 2 === 0 ? <Even /> : null}
+      </section>
+
+      <seciont>
+        <Controller onClickButton={onClickButton} />
+      </seciont>
+    </div>
+  );
+}
+
+export default App;
